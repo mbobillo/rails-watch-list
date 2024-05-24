9b211c74f8776327c4536ffc95689bcc14bb4fce
Marion Bobillo <marion.bobillo@gmail.com>
2024-05-24 11:19:43 +0200

    push before prod

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,40 @@
+class BookmarksController < ApplicationController
+  before_action :set_bookmark, only: %i[destroy]
+  before_action :set_list, only: %i[new create]
+
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to bookmarks_path, status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
